916020e9ea0d08524cb2fd637f5a2515ccc49afc
Sean Brody <ifonefox@gmail.com>
2019-12-03 22:04:26 -0500

    add shortcut for adding days

---
+++ Template/day0Tests/day3Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import day0lib
+
+final class day0Tests: XCTestCase {
+    func testParse() {
+        XCTAssertEqual(0,0);
+     }
+    static var allTests = [
+        ("testParse", testParse),
+    ]
+}
---
+++ add_day.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+cp -r Template/day0 Sources/day$1
+cp -r Template/day0lib Sources/day$1lib
+cp -r Template/day0Tests Tests/day$1Tests
